f5cf1d40bf9a48d1919db8d9b23c62c236bedd09
Olivier Louvignes <olivier@mg-crea.com>
2013-02-03 21:24:17 +0100

    chore(version): update to v0.6.4

--- js/app.js
+++ js/app.js
@@ -57,4 +57,8 @@ app.controller('StrapCtrl', function($scope) {
 	$scope.typeahead = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Dakota","North Carolina","Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming"];
 	$scope.datepicker = {date: ''};
 	$scope.timepicker = {time: ''};
+
+	$scope.prettyPrint = function() {
+		window.prettyPrint && window.prettyPrint();
+	}
 });
