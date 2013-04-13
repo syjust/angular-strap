7c45c39402118d2f7b6229b6b907559f632f6954
Olivier Louvignes <olivier@mg-crea.com>
2013-04-13 02:37:10 +0200

    chore(lib): update to latest v0.7.2

--- js/app.js
+++ js/app.js
@@ -7,9 +7,11 @@ app.controller('StrapCtrl', function($scope, $window, $location) {
 	$scope.$alert = $window.alert.bind(null);
 
 	// Modal directive
+  // http://plnkr.co/edit/xKP5PW3Dco1CczmCuuVN?p=preview
 	$scope.modal = {content: 'Hello Modal', saved: false};
 
 	// Dropdown directive
+  // http://plnkr.co/edit/ywCmR5wcMpZElwE5Qk9S?p=preview
 	$scope.dropdown = [
 		{text: 'Another action', href: '#anotherAction'},
 		{text: 'Something else here', click: "$alert('working ngClick!')"},
@@ -23,9 +25,11 @@ app.controller('StrapCtrl', function($scope, $window, $location) {
 	];
 
 	// Navbar directive
+  // http://plnkr.co/edit/G67eybC4zwlF1sqLH93t?p=preview
 	$scope.$location = $location;
 
 	// Tab directive
+  // http://plnkr.co/edit/3C9vTbdhLRGo3X2Nu0yw?p=preview
 	$scope.tabs = [
     {title:'Home', content: 'Raw denim you probably haven\'t heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica.'},
     {title:'Profile', content: 'Food truck fixie locavore, accusamus mcsweeney\'s marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee.'},
